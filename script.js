94b7a890487255cdb5d4e6422c1bcdacb165dd00
bigbossbry <brycmramirez@gmail.com>
2023-03-07 20:03:38 +0800

    Initial Skeleton and declare basic foundation functions.

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+const myLibrary = [];
+
 function Books(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
     this.info = function () {
-        return title + ' by ' + author + ', ' + pages + 'pages,' + readStatus;
+        return `${title  } by ${  author  }, ${  pages  }pages, ${  readStatus}`;
     }
 }
+
+function addToLibrary (book) {
+    myLibrary.push(book);
+}
+
+const witcher = new Books("The Witcher: The Last Wish", "Andrzej Sapkowski", "288", "Read");
+
+addToLibrary(witcher);
