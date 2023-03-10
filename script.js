8601ddc7687b23de17ac08d2bde2e7574e0701d9
bigbossbry <brycmramirez@gmail.com>
2023-03-10 16:14:16 +0800

    Created the initial constructor function, a function that pushes the books into an array object, and an iterator function that creates card items based on the contents of the array.

--- script.js
+++ script.js
@@ -16,4 +16,34 @@ function addToLibrary (book) {
 
 const witcher = new Books("The Witcher: The Last Wish", "Andrzej Sapkowski", "288", "Read");
 
+const book2 = new Books("Book2", "bigbossbry", "0", "Not Read");
+
+const book3 = new Books("Book3", "bigbossbry", "0", "Not Read");
+
 addToLibrary(witcher);
+addToLibrary(book2);
+addToLibrary(book3);
+
+// Looping function
+
+const cardContainer = document.querySelector('.library');
+
+function createCard(item) {
+    console.log (item); // Item here is the array index.
+    const cards = document.createElement('div'); // This is the div to append the items to.
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    title.textContent = item.title;
+    author.textContent = item.author;
+    pages.textContent = item.pages;
+    cards.appendChild(title);
+    cards.appendChild(author);
+    cards.appendChild(pages);
+    cards.classList.add('card');
+    cardContainer.appendChild(cards);
+}
+
+myLibrary.forEach(createCard)
+
+
