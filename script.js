3c65dd122e69d50f335483667c6934b6a484bd72
bigbossbry <brycmramirez@gmail.com>
2023-10-24 14:42:55 +0800

    Create a factory function 'createBook' effectively converting the constructor 'Books' into a factory

--- script.js
+++ script.js
@@ -6,8 +6,21 @@ function Books(title, author, pages, readStatus) {
     this.pages = pages;
     this.readStatus = readStatus;
     this.info = function () {
-        return `${title  } by ${  author  }, ${  pages  }pages, ${  readStatus}`;
+        return `${title} by ${author}, ${pages} pages, ${readStatus}`;
     }
+}
+
+
+// Convert the above constructor function into a factory function
+function createBook (title, author, pages, readStat) {
+    const bookTitle = title;
+    const bookAuthor = author;
+    const bookPages = pages;
+    const bookStat = readStat;
+    const bookInfo = function () {
+        return `${bookTitle} by ${bookAuthor}, ${bookPages} pages, ${bookStat}`
+    }
+    return {bookTitle, bookAuthor, bookPages, bookStat, bookInfo}
 }
 
 function addToLibrary (book) {
@@ -24,18 +37,23 @@ addToLibrary(witcher);
 addToLibrary(book2);
 addToLibrary(book3);
 
+
+const overlay = document.querySelector('.overlay');
+// overlay.addEventListener('click', fnplaceholder)
+
 // Looping function
 
-const cardContainer = document.querySelector('.library');
+const cardsContainer = document.querySelector('.library');
 
 function createCard(item) {
     console.log (item); // Item here is the array index.
     const cards = document.createElement('div'); // This is the div to append the items to.
+    cards.classList.add(`data-`);
     const title = document.createElement('p');
     const author = document.createElement('p');
     const pages = document.createElement('p');
-    const readStatus = document.createElement('p');
-    readStatus.classList.add('toggle-label');
+    const markRead = document.createElement('p');
+    markRead.classList.add('toggle-label');
     const label = document.createElement('label');
     label.classList.add('switch');
     const checkbox = document.createElement('input');
@@ -48,20 +66,72 @@ function createCard(item) {
     title.textContent = item.title;
     author.textContent = item.author;
     pages.textContent = item.pages;
-    readStatus.textContent = 'Mark as Read';
+    markRead.textContent = 'Mark as Read';
     remove.textContent = 'x';
     label.appendChild(checkbox);
     label.appendChild(box);
     cards.appendChild(title);
     cards.appendChild(author);
     cards.appendChild(pages);
-    cards.appendChild(readStatus);
+    cards.appendChild(markRead);
     cards.appendChild(label);
     cards.appendChild(remove);
     cards.classList.add('card');
-    cardContainer.appendChild(cards);
+    cardsContainer.appendChild(cards);
+    if (item.readStatus === true) {
+        checkbox.checked = true;
+    }
+}
+
+// Delete cards
+// CardContainer is the library div
+function deleteCards() {
+    while (cardsContainer.firstChild) {
+      cardsContainer.lastChild = null;
+      cardsContainer.removeChild(cardsContainer.lastChild);
+    }
+  }
+
+  
+// New card based on forms
+const form = document.querySelector('.new-book');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const readStatus = document.querySelector('#readStatus').checked;
+    const newBook = new Books(title, author, pages, readStatus);
+    addToLibrary(newBook);
+    deleteCards();
+    myLibrary.forEach(createCard)
+})
+
+function deleteThisCard (clickEvent) {
+    const parent = clickEvent.target.parentNode;
+    parent.remove();
+}
+
+function listenClick(target) {
+    target.addEventListener('mousedown',deleteThisCard);
 }
 
 myLibrary.forEach(createCard)
+const btnDelete = document.querySelectorAll('.remove');
+btnDelete.forEach(listenClick);
 
+const addNewBook = document.querySelector('.add-book');
 
+function toggleForm () {
+    const bookForm = document.querySelector('.form-hidden');
+    bookForm.classList.toggle('active');
+    overlay.classList.toggle('active');
+}
+
+const formClose = document.querySelector('.form-close');
+
+addNewBook.addEventListener('mousedown', toggleForm);
+formClose.addEventListener('mousedown', toggleForm);
+
+// Need to add a function to remove an object from the library array.
